1b3ff7df8ef30832084495ac4074585a685ff522
Anthony Kowalski <kowalsan@oregonstate.edu>
2023-06-05 16:37:01 -0700

    adding header/footer updates

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,37 @@
-const Header = (props) => { 
+const Header = () => { 
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <p>Use the links below to find the page you need:</p>
-      <nav>[ <a href="/cafes">Cafes</a> | <a href="./cafes_franchisees">Cafes and Franchisees</a> | <a href="/dues_owed">Dues Owed</a> | <a href="/franchisees">Franchisees</a> | <a href="/inventory_items">Inventory Items</a> |  <a href="/inventory_orders">Inventory Orders</a> | <a href="/sale_items">Sale Items</a> | <a href="/sales">Sales</a> ] </nav>
+    <div className="header-container">
+      <h1>Dutch Girls Coffee Database</h1>
+      <nav>
+        [ 
+          <button className="button-header" onClick={() => { window.location.href = "/cafes"; }}>
+            Cafes
+          </button> |
+          <button className="button-header" onClick={() => { window.location.href = "/cafes_franchisees"; }}>
+            Cafes and Franchisees
+          </button> |
+          <button className="button-header" onClick={() => { window.location.href = "/dues_owed"; }}>
+            Dues Owed
+          </button> |
+          <button className="button-header" onClick={() => { window.location.href = "/franchisees"; }}>
+            Franchisees
+          </button> |
+          <button className="button-header" onClick={() => { window.location.href = "/inventory_items"; }}>
+            Inventory Items
+          </button> |
+          <button className="button-header" onClick={() => { window.location.href = "/inventory_orders"; }}>
+            Inventory Orders
+          </button> |
+          <button className="button-header" onClick={() => { window.location.href = "/sale_items"; }}>
+            Sale Items
+          </button> |
+          <button className="button-header" onClick={() => { window.location.href = "/sales"; }}>
+            Sales
+          </button>
+        ]
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Header;
